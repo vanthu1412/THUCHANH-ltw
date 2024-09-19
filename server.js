970a2a90e63ef637d91f2fb3867194f3359684b5
vanthu1412 <ncvthu2101025@student.ctuet.edu.vn>
2024-09-19 15:53:04 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import dotenv from 'dotenv/config'
+import myDateTime from './date'
+import getURL from './getURL'
+import viewEngine from './viewEngine'
+
+const app = express()
+viewEngine(app)
+const port=process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send('Hello, My name is Baro')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/about', (req, res) => {
+    res.send('Xin chào, tên của tôi là Baro')
+})
+
+app.get('/date', (req, res) => {
+    const date = myDateTime();
+    res.send(date);
+});
+
+app.get('/geturl', (req, res) => {
+    const getParamsURL = getURL.getParamsURL(req);
+    res.send(getParamsURL);
+})
+
+app.get('/geturl', (req, res) => {
+    const getPath = getURL.getPath(req);
+    res.send(getPath);
+})
+
+app.get('/ejs', (req, res) => { 
+    res.render("test")
+})
+
+app.get('/homeejs', (req, res) => { 
+    res.render("home")
+})
+
+app.get('/aboutejs', (req, res) => { 
+    res.render("about")
+})
